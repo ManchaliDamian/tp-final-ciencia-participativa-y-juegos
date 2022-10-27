154b7b74123081a7c56f829157473bfa6f5f32a2
jaraJulieta <jarajuli13@gmail.com>
2022-10-26 22:44:45 -0300

    test usuario preguntar

---
+++ test/ar/edu/unq/po2/tp/Final/UsuarioTestCase.java
@@ -0,0 +1,31 @@
+package ar.edu.unq.po2.tp.Final;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class UsuarioTestCase {
+	Usuario usuario;
+	Preferencia preferencia; 
+	
+	@BeforeEach
+	public void setUp() throws Exception{
+		preferencia = mock(Preferencia.class); 
+		usuario = new Usuario(preferencia); 
+	}
+
+	@Test
+	void testUnUsuarioTieneUnaPreferenciaDeDesafío() {
+		
+	}
+	
+	@Test
+	void testUnUsuarioTieneUnaPreferenciaEnRecompesa() {
+		when(preferencia.getRecompensa()).thenReturn(20);
+		
+		assertEquals(20, usuario.getPreferencia().getRecompensa());
+	}
+
+}
